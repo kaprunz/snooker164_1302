ac0fab819db82aff04c4badd4664c3d03494b0e8
Jewelrypraline <yamerojellyfish@gmail.com>
2026-07-18 14:14:06 +0700

    test

---
+++ Assets/TutorialInfo/Scripts/Test/test.cs
@@ -0,0 +1,31 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private int n = 0;
+    private float timer = 0f;
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        n++;
+        Debug.Log(Time.deltaTime);
+        if (timer >= 1f)
+        {
+            Debug.Log(n);
+            timer = 0f;
+            n = 0;
+        }
+    }
+}
